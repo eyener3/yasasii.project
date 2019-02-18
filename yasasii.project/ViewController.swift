2f6a6b588427ebfaaf6847a3e95340925017a931
Ayana Kato <ayana20130321@gmail.com>
2019-02-18 15:57:54 +0900

    Initial Commit

---
+++ yasasii.project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  yasasii.project
+//
+//  Created by 加藤彩那 on 2019/02/18.
+//  Copyright © 2019 加藤彩那. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
